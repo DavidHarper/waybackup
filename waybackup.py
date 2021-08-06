6b8fd7498e0c625ae38af1c8cf25393a9c96695e
David Harper <david@obliquity.net>
2021-08-06 11:11:49 +0100

    Preliminary object-oriented version.

--- waybackup.py
+++ waybackup.py
@@ -2,196 +2,194 @@
 
 import sys, os, stat, hashlib
 from datetime import datetime
+from enum import Enum
 
-debug=os.getenv('DEBUG') != None
+class WayBackupEvent(Enum):
+    STARTED_BACKUP = 1
+    ENTERED_DIRECTORY = 2
+    SKIPPED_DIRECTORY = 3
+    COPIED_FILE = 4
+    COPIED_SYMLINK = 5
+    COPIED_ATTRIBUTES = 6
+    CREATED_LINK = 7
+    EXITED_DIRECTORY = 8
+    FINISHED_BACKUP = 9
 
-IGNORE_FILE_NAME='.waybackup.ignore'
+class WayBackup:
+    IGNORE_FILE_NAME='.waybackup.ignore'
 
-directories_traversed = 0
-directories_skipped=0
-files_copied = 0
-files_bytes_copied=0
-file_attributes_copied = 0
-symlinks_copied = 0
-links_created = 0
+    def __init__(self, dryrun=False, verbose=False, callback=None):
+        self.dryrun=dryrun
+        self.verbose=verbose
+        self.callback=callback
 
-def traverser(srcdir, refdir, tgtdir, ignore=None):
-    global directories_traversed, directories_skipped
+    def backup(self, srcdir, refdir, tgtdir):
+        self.directories_processed=0
+        self.directories_skipped=0
+        self.files_copied=0
+        self.files_bytes_copied=0
+        self.file_attributes_copied=0
+        self.symlinks_copied=0
+        self.links_created=0
 
-    if debug:
-        print('\n### ENTER traverser(' + srcdir + ', ' + refdir + ', ' + tgtdir
-        + ', ignore=' + str(ignore) + ')')
+        if self.callback is not None:
+            self.callback(WayBackupEvent.STARTED_BACKUP, None)
 
-    if (ignore is not None) and (srcdir in ignore):
-        directories_skipped = directories_skipped + 1
-        if debug:
-            print('# Skipping ' + srcdir + ' because it is in the ignore list.')
-        return
+        dtStart=datetime.now()
 
-    if debug:
-        print("#! mkdir " + tgtdir)
+        process_directory(self, srcdir, refdir, tgtdir)
 
-    os.makedirs(tgtdir)
+        dtFinish=datetime.now()
 
-    ignore=update_ignore_list(ignore, srcdir)
+        if self.callback is not None:
+            results = {
+                'dryrun' : self.dryrun,
+                'time' : dtFinish-dtStart,
+                'directories_processed' : self.directories_processed ,
+                'directories_skipped' : self.directories_skipped,
+                'files_copied' : self.files_copied,
+                'bytes_copied' : self.bytes_copied,
+                'file_attributes_copied' : self.file_attributes_copied,
+                'symlinks_copied' : self.symlinks_copied,
+                'links_created' : self.links_created
+            }
 
-    for file in os.listdir(srcdir):
-        srcpath = os.path.join(srcdir, file)
-        refpath = os.path.join(refdir, file)
-        tgtpath = os.path.join(tgtdir, file)
-        if os.path.islink(srcpath) or os.path.isfile(srcpath):
-            process_file(srcpath, refpath, tgtpath)
-        elif os.path.isdir(srcpath):
-            traverser(srcpath, refpath, tgtpath, ignore)
-        else:
-            process_other_entity(srcpath, refpath, tgtpath)
+            self.callback(WayBackupEvent.FINISHED_BACKUP, results)
 
-    copy_file_attributes(srcdir, tgtdir)
+    def process_directory(self, srcdir, refdir, tgtdir, ignore=None):
+        ignore=update_ignore_list(self, ignore, srcdir)
 
-    directories_traversed = directories_traversed + 1
+        if (ignore is not None) and (srcdir in ignore):
+            if self.callback is not None:
+                self.callback(WayBackupEvent.SKIPPED_DIRECTORY, {'name' : srcdir})
+            return None
 
-def update_ignore_list(ignore, srcdir):
-    ignorefile=os.path.join(srcdir, IGNORE_FILE_NAME)
+        if self.callback is not None:
+            self.callback(WayBackupEvent.ENTERED_DIRECTORY, {'name' : srcdir})
 
-    if not os.path.isfile(ignorefile):
-        return ignore
+        if not self.dryrun:
+            os.makedirs(tgtdir)
 
-    ignorelist=[]
+        for file in os.listdir(srcdir):
+            srcpath = os.path.join(srcdir, file)
+            refpath = os.path.join(refdir, file)
+            tgtpath = os.path.join(tgtdir, file)
 
-    with open(ignorefile, 'r') as f:
-        for line in f:
-            path=line.strip('\n')
-            if os.path.isabs(path):
-                print('WARNING: Ignoring absolute path in ignore file: ' + path)
+            if os.path.islink(srcpath) or os.path.isfile(srcpath):
+                process_file(self, srcpath, refpath, tgtpath)
+            elif os.path.isdir(srcpath):
+                process_directory(self, srcpath, refpath, tgtpath, ignore)
             else:
-                abspath=os.path.join(srcdir,path)
-                ignorelist.append(abspath)
+                process_other_entity(self, srcpath, refpath, tgtpath)
 
-    if len(ignorelist)==0:
-        return ignore
+        copy_file_attributes(self, srcdir, tgtdir)
 
-    if debug:
-        print('# Adding to ignore list: ' + str(ignorelist))
+        if self.callback is not None:
+            self.callback(WayBackupEvent.EXITED_DIRECTORY, {'name' : srcdir})
 
-    if ignore is None:
-        return set(ignorelist)
-    else:
-        return ignore | set(ignorelist)
+    def update_ignore_list(self, ignore, srcdir):
+        ignorefile=os.path.join(srcdir, IGNORE_FILE_NAME)
 
-def process_file(srcpath, refpath, tgtpath):
-    global links_created
+        if not os.path.isfile(ignorefile):
+            return ignore
 
-    if debug:
-        print("\n### ENTER process_file(" + srcpath + ", " + refpath  + ", " + tgtpath+ ")")
+        ignorelist=[]
 
-    if not os.path.exists(refpath):
-        if debug:
-            print("# refpath does not exist")
-        copy_file(srcpath, tgtpath)
+        with open(ignorefile, 'r') as f:
+            for line in f:
+                path=line.strip('\n')
+                if not os.path.isabs(path):
+                    abspath=os.path.join(srcdir,path)
+                    ignorelist.append(abspath)
+
+        if len(ignorelist)==0:
+            return ignore
+
+        if ignore is None:
+            return set(ignorelist)
+        else:
+            return ignore | set(ignorelist)
+
+    def process_file(self, srcpath, refpath, tgtpath):
+        if not os.path.exists(refpath) or not os.path.isfile(refpath):
+            copy_file(self, srcpath, tgtpath)
+            return
+
+        srcstat = os.stat(srcpath)
+        refstat = os.stat(refpath)
+
+        if srcstat.st_mtime > refstat.st_mtime or srcstat.st_size != refstat.st_size:
+            copy_file(self, srcpath, tgtpath)
+            return
+
+        if not self.dryrun:
+            os.link(refpath, tgtpath)
+
+        self.links_created = self.links_created + 1
+
+        if self.verbose and self.callback is not None:
+            self.callback(WayBackupEvent.CREATED_LINK, {'name' : srcpath})
+
+        if srcstat.st_ctime > refstat.st_ctime or srcstat.st_mode != refstat.st_mode
+        or srcstat.st_uid != refstat.st_uid or srcstat.st_gid != refstat.st_gid:
+            copy_file_attributes(self, srcpath, tgtpath)
+            if self.verbose and self.callback is not None:
+                self.callback(WayBackupEvent.COPIED_ATTRIBUTES, {'name' : srcpath})
+
+    def process_other_entity(self, srcpath, refpath, tgtpath):
         return
 
-    if not os.path.isfile(refpath):
-        if debug:
-            print("# refpath is not a regular file")
-        copy_file(srcpath, tgtpath)
-        return
+    def calculate_md5_of_file(path):
+        with open(path, "rb") as f:
+            file_hash = hashlib.md5()
+            while chunk := f.read(8192):
+                file_hash.update(chunk)
 
-    srcstat = os.stat(srcpath)
-    refstat = os.stat(refpath)
+        return file_hash.hexdigest()
 
-    if srcstat.st_mtime > refstat.st_mtime:
-        if debug:
-            print("# mtime more recent")
-        copy_file(srcpath, tgtpath)
-        return
+    def copy_file_attributes(self, srcpath, tgtpath):
+        if not self.dryrun:
+            srcstat = os.stat(srcpath)
+            os.chown(tgtpath, srcstat.st_uid, srcstat.st_gid)
+            os.chmod(tgtpath, srcstat.st_mode)
+            os.utime(tgtpath, ns=(srcstat.st_atime_ns,srcstat.st_mtime_ns))
 
-    if srcstat.st_size != refstat.st_size:
-        if debug:
-            print("# size mismatch")
-        copy_file(srcpath, tgtpath)
-        return
+        if os.path.isfile(tgtpath):
+            self.file_attributes_copied = self.file_attributes_copied + 1
 
-    if srcstat.st_ctime > refstat.st_ctime:
-        if debug:
-            print("# ctime more recent")
-        link_and_set_attributes(srcpath, refpath, tgtpath)
-        return
+    def copy_file(srcpath, tgtpath, chunksize = 8192):
+        if os.path.islink(srcpath):
+            if not self.dryrun:
+                os.symlink(os.readlink(srcpath), tgtpath)
 
-    if srcstat.st_mode != refstat.st_mode:
-        if debug:
-            print("# mode mismatch")
-        link_and_set_attributes(srcpath, refpath, tgtpath)
-        return
+            self.symlinks_copied = self.symlinks_copied + 1
 
-    if srcstat.st_uid != refstat.st_uid:
-        if debug:
-            print("# uid mismatch")
-        link_and_set_attributes(srcpath, refpath, tgtpath)
-        return
+            if self.verbose and self.callback is not None:
+                self.callback(WayBackupEvent.CREATED_SYMLINK, {'name' : srcpath})
 
-    if srcstat.st_gid != refstat.st_gid:
-        if debug:
-            print("# gid mismatch")
-        link_and_set_attributes(srcpath, refpath, tgtpath)
-        return
+            return
+        else:
+            if not self.dryrun:
+                with open(srcpath, "rb") as fsrc, open(tgtpath, "wb") as ftgt:
+                    while chunk := fsrc.read(chunksize):
+                        ftgt.write(chunk)
 
-    if debug:
-        print("# no change\n#! ln " + refpath + " " + tgtpath)
+            copy_file_attributes(self, srcpath, tgtpath)
 
-    os.link(refpath, tgtpath)
+            srcstat = os.stat(srcpath)
+            self.files_bytes_copied = self.files_bytes_copied + srcstat.st_size
+            self.files_copied = self.files_copied + 1
 
-    links_created = links_created + 1
+            if self.verbose and self.callback is not None:
+                self.callback(WayBackupEvent.COPIED_FILE, {'name' : srcpath, 'size' : srcstat.st_size})
 
-def process_other_entity(srcpath, refpath, tgtpath):
-    return
-
-def calculate_md5_of_file(path):
-    with open(path, "rb") as f:
-        file_hash = hashlib.md5()
-        while chunk := f.read(8192):
-            file_hash.update(chunk)
-
-    return file_hash.hexdigest()
-
-def copy_file_attributes(srcpath, tgtpath):
-    global file_attributes_copied
-
-    if debug:
-        print('#! copyattrs ' + srcpath + ' ' + tgtpath)
-    srcstat = os.stat(srcpath)
-    os.chown(tgtpath, srcstat.st_uid, srcstat.st_gid)
-    os.chmod(tgtpath, srcstat.st_mode)
-    os.utime(tgtpath, ns=(srcstat.st_atime_ns,srcstat.st_mtime_ns))
-
-    if os.path.isfile(tgtpath):
-        file_attributes_copied = file_attributes_copied + 1
-
-def copy_file(srcpath, tgtpath, chunksize = 8192):
-    global files_copied, symlinks_copied, files_bytes_copied
-
-    if os.path.islink(srcpath):
-        if debug:
-            print("#! ln -s  " + os.readlink(srcpath) + ' ' + tgtpath)
-        os.symlink(os.readlink(srcpath), tgtpath)
-        symlinks_copied = symlinks_copied + 1
-    else:
-        if debug:
-            print('#! cp -p ' + srcpath + ' ' + tgtpath)
-        with open(srcpath, "rb") as fsrc, open(tgtpath, "wb") as ftgt:
-            while chunk := fsrc.read(chunksize):
-                ftgt.write(chunk)
-                files_bytes_copied = files_bytes_copied + len(chunk)
-
-        files_copied = files_copied + 1
-
-        copy_file_attributes(srcpath, tgtpath)
-
-def link_and_set_attributes(srcpath, refpath, tgtpath):
-    if debug:
-        print('#! ln ' + srcpath + ' ' + tgtpath)
-    os.link(refpath, tgtpath)
-    copy_file_attributes(srcpath, tgtpath)
-
+def reporter(event_type, event_dict):
+    print(event_type.name)
+    if event_dict is not None:
+        for key in event_dict:
+            print('\t',key,' ==> ',str(event_dict[key]))
+    print()
+    
 if __name__ == '__main__':
     if len(sys.argv) < 4:
         print("Usage: " + sys.argv[0] + " source-directory reference-directory target-directory")
@@ -209,21 +207,6 @@ if __name__ == '__main__':
         print("Target directory " + tgtdir + " is not empty ... bailing out!")
         exit(3)
 
-    dtStart=datetime.now()
+    backup=WayBackup(callback=reporter, verbose=True)
 
-    traverser(srcdir, refdir, tgtdir)
-
-    dtFinish=datetime.now()
-
-    elapsed='%.3f' % (dtFinish.timestamp()-dtStart.timestamp())
-
-    filebytes="{:,}".format(files_bytes_copied)
-
-    print("WAYBACKUP RUN FINISHED")
-    print("Started at " + str(dtStart) + ", finished at " + str(dtFinish) + " (" + elapsed + " seconds)")
-    print("Traversed " + str(directories_traversed) + " directories")
-    print("Skipped " + str(directories_skipped) + " directories")
-    print("Linked " + str(links_created) + " files")
-    print("Copied " + str(files_copied) + " files (" + filebytes + " bytes)")
-    print("Copied " + str(symlinks_copied) + " symlinks")
-    print("Altered " + str(file_attributes_copied) + " sets of file attributes")
+    backup.backup(srcdir, refdir, tgtdir)
